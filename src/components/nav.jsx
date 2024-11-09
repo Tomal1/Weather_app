34d9d84859753be7442390982e14dd7b6276c926
MI <minhazulislam@MI.local>
2024-11-09 13:45:45 +0000

    begining of nav bar

---
+++ src/components/nav.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "../assets/style/Nav.css"
+
+
+const Nav = () =>{
+    return(
+        <>
+                <p>todays weather</p>
+                
+                <label>search location</label>
+                <form>
+                    <input type="submut"/>
+                    <input type="button" value="search"/>
+
+                </form>
+                
+        
+        </>
+
+    )
+
+}
+
+export default Nav

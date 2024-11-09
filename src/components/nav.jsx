95438a867ac203af26dfef68729c56c4735359ec
MI <minhazulislam@MI.local>
2024-11-09 16:03:03 +0000

    search bar intergrated into navbar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,19 +1,29 @@
 import React from "react";
+import { useState } from "react";
 
 import "../assets/style/Nav.css"
 
 
 const Nav = () =>{
+
+    const [location, setLocation] = useState("")
+
+    const handleSearch = (e) =>{
+        e.prevantDefault()
+    }
+    
     return(
         <>
                 <p>todays weather</p>
                 
+                <div className="searchBox">
                 <label>search location</label>
-                <form>
-                    <input type="submut"/>
-                    <input type="button" value="search"/>
-
-                </form>
+                    <form>
+                        
+                        <input type="text" value={location} onChange={(e)=>setLocation(e.target.value)}/>
+                        <input type="button" value="search" onClick={() =>handleSearch(e)}/>
+                    </form>
+                </div>
                 
         
         </>

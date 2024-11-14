2a43ec3fbf5d70be25018094904dc5a4ef750bec
MI <minhazulislam@hotmail.com>
2024-11-14 20:16:52 +0000

    logic found to store data from fetch into a global variable

--- src/components/current.jsx
+++ src/components/current.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "../assets/style/current.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Current = () => {
   let lat;
   let long;
+ 
+
   const CurrentLocation = () => {
     const success = (position) => {
       lat = position.coords.latitude;
@@ -15,33 +19,39 @@ const Current = () => {
     navigator.geolocation.getCurrentPosition(success, error);
   };
 
-  
- 
+
+
+  let [currentLoc, setCurrentLoc] = useState(null);
+
+  useEffect(() => {
     fetch(
       `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=7&appid=1c2664e16cd9dca0ca2c91a78a16c059`
     )
       .then((res) => res.json())
-      .then((data) => {
+      .then((data) => setCurrentLoc(data[0].name))
+      .catch((err)=> console.error(err))
+  });
 
-        data[0].name
-        fetch(
-          `https://api.openweathermap.org/data/2.5/forecast?q=${data[0].name}&appid=1c2664e16cd9dca0ca2c91a78a16c059`
-        )
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data);
-            //code stars hear
-          });
-      })
+
+
+  // fetch(
+  //   `https://api.openweathermap.org/data/2.5/forecast?q=${data[0].name}&appid=1c2664e16cd9dca0ca2c91a78a16c059`
+  // )
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //     //code stars hear
+  //   });
+
 
   return (
     <div id="currentCon">
       <div className="display">
         <p>{CurrentLocation()}</p>
+        <p>yess yess {currentLoc && console.log(currentLoc)}</p>
       </div>
     </div>
-    
-)
+  );
 };
 
 export default Current;

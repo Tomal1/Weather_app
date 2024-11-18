0f41a0697e974c93cf49d3628d114da28b61fdc7
MI <minhazulislam@hotmail.com>
2024-11-18 22:29:26 +0000

    variables for differet atrubutes

--- src/components/current.jsx
+++ src/components/current.jsx
@@ -12,7 +12,7 @@ const Current = () => {
     const success = (position) => {
       lat = position.coords.latitude;
       long = position.coords.longitude;
-      fetchName();
+   
     };
 
     const error = () => alert("please turn on your location");
@@ -21,34 +21,66 @@ const Current = () => {
 
 
 
-  let [currentLoc, setCurrentLoc] = useState(null);
+//   let [currentLoc, setCurrentLoc] = useState(null);
 
-  useEffect(() => {
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=7&appid=1c2664e16cd9dca0ca2c91a78a16c059`
+//   useEffect(() => {
+//     fetch(
+//       `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=7&appid=1c2664e16cd9dca0ca2c91a78a16c059`
+//     )
+//       .then((res) => res.json())
+//       .then((data) => setCurrentLoc(data[0].name))
+//       .catch((err)=> console.error(err))
+//   },[]);
+
+//   currentLoc && console.log(currentLoc)
+
+
+  let [searchData, setSearchData] = useState(null)
+
+
+  const searchLoc =()=>{
+    // renderCurrent()
+    
+    
+    useEffect(() =>{
+        fetch(
+            `https://api.openweathermap.org/data/2.5/forecast?q=dudley&appid=1c2664e16cd9dca0ca2c91a78a16c059`
+          )
+            .then((res) => res.json())
+            .then((data) => setSearchData(data));
+      },[])
+  }
+
+
+
+  let name = searchData.city.name
+  let maxT = searchData.list[0].main.temp_max
+  let minT = searchData.list[0].main.temp_max
+  let humidity = searchData.list[0].main.humidity
+  let pressure = searchData.list[0].main.pressure
+  let discription = searchData.list[0].weather[0].discription
+  let icon = searchData.list[0].weather[0].icon
+  let windObj= searchData.list[0].wind
+  let dt = searchData.list[0].dt
+
+  const renderCurrent = ()=>{
+
+    return (
+        <div>hell</div>
+
     )
-      .then((res) => res.json())
-      .then((data) => setCurrentLoc(data[0].name))
-      .catch((err)=> console.error(err))
-  });
 
+  }
 
-
-  // fetch(
-  //   `https://api.openweathermap.org/data/2.5/forecast?q=${data[0].name}&appid=1c2664e16cd9dca0ca2c91a78a16c059`
-  // )
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     console.log(data);
-  //     //code stars hear
-  //   });
 
 
   return (
     <div id="currentCon">
       <div className="display">
         <p>{CurrentLocation()}</p>
-        <p>yess yess {currentLoc && console.log(currentLoc)}</p>
+        <p>{searchLoc()}</p>
+        
+        
       </div>
     </div>
   );

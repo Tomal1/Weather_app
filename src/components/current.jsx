4f8dad7dc12ebf7bd9a6502295f6430a1efaf091
MI <minhazulislam@hotmail.com>
2024-11-23 16:49:45 +0000

    fetch api has been set up to start rendering current weater values

--- src/components/current.jsx
+++ src/components/current.jsx
@@ -6,81 +6,69 @@ import { useEffect } from "react";
 const Current = () => {
   let lat;
   let long;
- 
 
-  const CurrentLocation = () => {
+  const latLong = () => {
     const success = (position) => {
       lat = position.coords.latitude;
       long = position.coords.longitude;
-   
+
+      findCurrentLocation();
     };
 
     const error = () => alert("please turn on your location");
     navigator.geolocation.getCurrentPosition(success, error);
   };
 
+  let [currentLoc, setCurrentLoc] = useState(null);
 
-
-//   let [currentLoc, setCurrentLoc] = useState(null);
-
-//   useEffect(() => {
-//     fetch(
-//       `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=7&appid=1c2664e16cd9dca0ca2c91a78a16c059`
-//     )
-//       .then((res) => res.json())
-//       .then((data) => setCurrentLoc(data[0].name))
-//       .catch((err)=> console.error(err))
-//   },[]);
-
-//   currentLoc && console.log(currentLoc)
-
-
-  let [searchData, setSearchData] = useState(null)
-
-
-  const searchLoc =()=>{
-    // renderCurrent()
-    
-    
-    useEffect(() =>{
-        fetch(
-            `https://api.openweathermap.org/data/2.5/forecast?q=dudley&appid=1c2664e16cd9dca0ca2c91a78a16c059`
-          )
-            .then((res) => res.json())
-            .then((data) => setSearchData(data));
-      },[])
-  }
-
-
-
-  let name = searchData.city.name
-  let maxT = searchData.list[0].main.temp_max
-  let minT = searchData.list[0].main.temp_max
-  let humidity = searchData.list[0].main.humidity
-  let pressure = searchData.list[0].main.pressure
-  let discription = searchData.list[0].weather[0].discription
-  let icon = searchData.list[0].weather[0].icon
-  let windObj= searchData.list[0].wind
-  let dt = searchData.list[0].dt
-
-  const renderCurrent = ()=>{
-
-    return (
-        <div>hell</div>
-
+  const findCurrentLocation = () => {
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=7&appid=1c2664e16cd9dca0ca2c91a78a16c059`
     )
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrentLoc(data[0].name);
+      })
+      .catch((err) => console.error(err));
 
-  }
+
+  };
+
+// 86400 sec in 1 day
+// 3600 sec's in 1 hour
+//unix time is sec's since 01/01/1970
+// dt /86400 = nomber of days since 01/01/1970
+  let [info, setInfo] = useState(null)
+
+  useEffect(()=>{
+    currentLoc &&
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=dudley&appid=1c2664e16cd9dca0ca2c91a78a16c059`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        data && setInfo(data)
+        console.log("cisty name", data.city.name )
+        console.log("forcast until",((data.list[0].dt % 86400) / 86400) * 24 +":00")
+        console.log("Current temp",(Math.round(data.list[0].main.temp-273.15))+" C")
+        console.log("max",(Math.round(data.list[0].main.temp_max-273.15))+" C")
+        console.log("min",(Math.round(data.list[0].main.temp_min-273.15))+" C")
+        console.log("discription:",data.list[0].weather[0].description)
+        console.log("icon",data.list[0].weather[0].icon)
+        console.log("wind-speed:",data.list[0].wind.speed +" mph")
+      });
+ 
+  },[])
 
 
+//info.city.name
 
   return (
     <div id="currentCon">
       <div className="display">
-        <p>{CurrentLocation()}</p>
-        <p>{searchLoc()}</p>
-        
-        
+        <div>{latLong()}</div> {/*this will display current area only */}
+        <div>{console.log(info)}</div>
+        <div></div>
       </div>
     </div>
   );

f8d834ae7479a672577818dd2758c1681342526e
MI <minhazulislam@hotmail.com>
2024-11-24 20:45:24 +0000

    displaying the correct time

--- src/components/current.jsx
+++ src/components/current.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 const Current = () => {
-  let [currentLoc, setCurrentLoc] = useState([{}]);
+  let [currentLoc, setCurrentLoc] = useState(null);
 
   // if you want to avoid infinate loops put everything inside the use effect
   useEffect(() => {
@@ -22,7 +22,7 @@ const Current = () => {
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=1c2664e16cd9dca0ca2c91a78a16c059`
       )
         .then((res) =>  res.status !== 200 ? console.log("somthing wrong") : res.json())
-        .then((data) => setCurrentLoc(data))
+        .then((data) => data.cod === 200 && setCurrentLoc(data))
         .catch((err) => console.error(err));
     };
   }, []);
@@ -35,6 +35,8 @@ const Current = () => {
   // let max = (currentLoc.main.temp_max -273.15) + " C"
   // let description = currentLoc.weather[0].description
   // let icon = currentLoc.weather[0].icon
+  let d = new Date();
+  let time = d.toLocaleTimeString()
   console.log(currentLoc)
 
   // 86400 sec in 1 day
@@ -45,15 +47,15 @@ const Current = () => {
   return (
     <div id="currentCon">
       <div className="display">
+        {/* to refresh comment out image line first, then the rest, then uncomment everything except image and then uncomment image last */}
       <div className="location content">{currentLoc.name}</div>
-      <div className="time content">Forcast until: {(currentLoc.dt % 86400 / 86400 * 24).toFixed(2)}</div>
+      <div className="time content">{time}</div>
       <div className="currentTemp content">{Math.round(currentLoc.main.temp - 273.15) + " C"}</div>
       <div className="max content">{Math.round(currentLoc.main.temp_max -273.15) + " C"}</div>
       <div className="min content">{Math.round(currentLoc.main.temp_min -273.15) + " C"}</div>
       <div className="wind content">{currentLoc.wind.speed + " mph"}</div>
       <div className="discription content">{currentLoc.weather[0].description}</div>
-        {/* 
-        <div className="icon content">xxx</div>  */}
+      <div className="icon content"><img className="iconImg" src={`http://openweathermap.org/img/wn/${currentLoc.weather[0].icon}.png`}/></div> 
       </div>
     </div>
   );

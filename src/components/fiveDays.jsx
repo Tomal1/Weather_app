a2477cd56272bde096526a511dd1010f6ff35ea6
MI <minhazulislam@hotmail.com>
2024-11-28 22:05:37 +0000

    5 day forcast data displaying on screen

--- src/components/fiveDays.jsx
+++ src/components/fiveDays.jsx
@@ -1,75 +1,54 @@
 import React from "react";
-import "../assets/style/fiveDays.css"
+import "../assets/style/fiveDays.css";
 import { useEffect } from "react";
 import { useState } from "react";
 
-const FiveDays = (props) =>{
-
+const FiveDays = (props) => {
   let [info, setInfo] = useState(null);
 
-
-useEffect(()=>{
+  useEffect(() => {
     fetch(
       `https://api.openweathermap.org/data/2.5/forecast?q=${props.location}&appid=1c2664e16cd9dca0ca2c91a78a16c059`
     )
-      .then((res) => res.json())
+
+    
+      .then((res) => res.status !== 200 ? console.log("somthing wrong") : res.json())
       .then((data) => {
-        
 
-        let forcast = data.list
+        let forcast = data.list;
 
-        let one = []
+        let one = [];
 
-        for(let i=7; i<=42; i=i+8){
-
-          one.push(forcast[i])
+        for (let i = 7; i <= 42; i = i + 8) {
+          one.push(forcast[i]);
         }
 
-        setInfo(one)
-      })
+        forcast && setInfo(one);
+      });
+  }, []);
 
-  },[])
-
-
-
-
+  const display = info && info.map((x,i)=>{
+    // console.log(x.weather[i].icon)
 
     return(
-            <div id="fiveDay">
+      <div className="day1 day">
+        <div className="date">date</div>
+         <div className="icon1"><img src={`http://openweathermap.org/img/wn/${x.weather[0].icon}.png`}/></div>
+        <div className="maxTemp">{Math.round(x.main.temp_max-273.15)+"C"}</div>
+        <div className="minTemp">{Math.round(x.main.temp_min-273.15)+"C"}</div>
+      </div>)
 
-              <div class="day1 day">
-                <div class="date">date</div>
-                <div class="icon1">image</div>
-                <div class="maxTemp">max</div>
-                <div class="minTemp">min</div>
-              </div>
-              <div class="day2 day">
-                <div class="date">date</div>
-                <div class="icon1">image</div>
-                <div class="maxTemp">max</div>
-                <div class="minTemp">min</div>
-              </div>
-              <div class="day3 day">
-                <div class="date">date</div>
-                <div class="icon1">image</div>
-                <div class="maxTemp">max</div>
-                <div class="minTemp">min</div>
-              </div>
-              <div class="day4 day">
-                <div class="date">date</div>
-                <div class="icon1"> image</div>
-                <div class="maxTemp">max</div>
-                <div class="minTemp">min</div>
-              </div>
-              <div class="day5 day">
-                <div class="date">date</div>
-                <div class="icon1">image</div>
-                <div class="maxTemp">max</div>
-                <div class="minTemp">min</div>
-              </div>
-            </div>
-            )
 
-}
+  })
 
-export default FiveDays
+  return (
+    <div id="fiveDay">
+      <ul>
+      {info && display}
+      </ul>
+      
+    </div>
+  );
+};
+
+export default FiveDays;

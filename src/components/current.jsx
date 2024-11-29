04358103592ebfc41a1a96a8dbe9b9a99a0ce995
MI <minhazulislam@hotmail.com>
2024-11-29 00:01:09 +0000

    description showing

--- src/components/current.jsx
+++ src/components/current.jsx
@@ -38,7 +38,6 @@ const Current = () => {
   // let icon = currentLoc.weather[0].icon
   let d = new Date();
   let time = d.toLocaleTimeString()
-  console.log(currentLoc)
 
   // 86400 sec in 1 day
   // 3600 sec's in 1 hour
@@ -47,20 +46,20 @@ const Current = () => {
 
 
 
-  return (
+  return currentLoc && (
     <div id="currentCon">
       <div className="display">
         {/* to refresh comment out image line first, then the rest, then uncomment everything except image and then uncomment image last */}
-        <div className="location content">{currentLoc && currentLoc.name}</div>
+        <div className="location content">{currentLoc.name}</div>
         <div className="time content">{time}</div>
-        <div className="currentTemp content">{currentLoc && Math.round(currentLoc.main.temp - 273.15) + " C"}</div>
-        <div className="max content">{currentLoc && Math.round(currentLoc.main.temp_max -273.15) + " C"}</div>
-        <div className="min content">{currentLoc && Math.round(currentLoc.main.temp_min -273.15) + " C"}</div>
-        <div className="wind content">{currentLoc && currentLoc.wind.speed + " mph"}</div>
-        <div className="discription content">{currentLoc && currentLoc.weather[0].description}</div>
-        <div className="icon content"><img className="iconImg" src={`http://openweathermap.org/img/wn/${currentLoc && currentLoc.weather[0].icon}.png`}/></div> 
+        <div className="currentTemp content">{Math.round(currentLoc.main.temp - 273.15) + " C"}</div>
+        <div className="max content">{Math.round(currentLoc.main.temp_max -273.15) + " C"}</div>
+        <div className="min content">{Math.round(currentLoc.main.temp_min -273.15) + " C"}</div>
+        <div className="wind content">{currentLoc.wind.speed + " mph"}</div>
+        <div className="discription content">{currentLoc.weather[0].description}</div>
+        <div className="icon content"><img className="iconImg" src={`http://openweathermap.org/img/wn/${currentLoc.weather[0].icon}.png`}/></div> 
       </div>
-        {currentLoc ? <FiveDays location={currentLoc.name}/>: ""}
+        {<FiveDays location={currentLoc.name}/>}
     </div>
 
 
--- src/components/fiveDays.jsx
+++ src/components/fiveDays.jsx
@@ -10,11 +10,11 @@ const FiveDays = (props) => {
     fetch(
       `https://api.openweathermap.org/data/2.5/forecast?q=${props.location}&appid=1c2664e16cd9dca0ca2c91a78a16c059`
     )
-
-    
-      .then((res) => res.status !== 200 ? console.log("somthing wrong") : res.json())
+      .then((res) =>
+        res.status !== 200 ? console.log("somthing wrong") : res.json()
+      )
       .then((data) => {
-        console.log(data)
+        console.log(data);
 
         let forcast = data.list;
 
@@ -28,27 +28,33 @@ const FiveDays = (props) => {
       });
   }, []);
 
-  const display = info && info.map((x,i)=>{
+  const display =
+    info &&
+    info.map((x, i) => {
+      let date = x.dt_txt.split(" ");
+      let date2 = date[0].split("-");
 
-    let date = x.dt_txt.split(" ")
-    let date2 =date[0].split("-")
+      return (
+        <div className="day">
+          <div className="date">{`${date2[2]}-${date2[1]}`}</div>
+          <div className="icon1">
+            <img
+              src={`http://openweathermap.org/img/wn/${x.weather[0].icon}.png`}
+            />
+          </div>
+          <div className="descr">{x.weather[0].description}</div>
+          <div className="maxTemp">
+            {Math.round(x.main.temp_max - 273.15) + " C"}
+          </div>
+          <div className="minTemp">
+            {Math.round(x.main.temp_min - 273.15) + " C"}
+          </div>
+        </div>
+      );
+    });
 
-    return(
-      <div className="day">
-        <div className="date">{`${date2[2]}-${date2[1]}`}</div>
-         <div className="icon1"><img src={`http://openweathermap.org/img/wn/${x.weather[0].icon}.png`}/></div>
-        <div className="maxTemp">{Math.round(x.main.temp_max-273.15)+"C"}</div>
-        <div className="minTemp">{Math.round(x.main.temp_min-273.15)+"C"}</div>
-      </div>)
-
-
-  })
-
-  return (
-    <div id="fiveDay">
-      {info && display}
-    </div>
-  );
+  return <div id="fiveDay">
+    {info && display}</div>;
 };
 
 export default FiveDays;

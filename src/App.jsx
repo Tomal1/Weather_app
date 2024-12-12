527522096f4e6cb17ab30ada4ee3b1c750e9d18d
MI <minhazulislam@hotmail.com>
2024-12-12 20:15:39 +0000

    url changed to filename

--- src/App.jsx
+++ src/App.jsx
@@ -7,46 +7,46 @@ import { useEffect } from "react";
 import "leaflet/dist/leaflet.css";
 
 function App() {
-  let [url, setUrl] = useState("");
+  let [filename, setFilename] = useState("");
 
   const backGround = (data) => {
     if (data === "01d") {
-      setUrl("01d.jpg");
+      setFilename("01d.jpg");
     } else if (data === "01n") {
-      setUrl("01n.jpg");
+      setFilename("01n.jpg");
     } else if (data === "02d") {
-      setUrl("02d.jpeg");
+      setFilename("02d.jpeg");
     } else if (data === "02n") {
-      setUrl("02n.jpeg");
+      setFilename("02n.jpeg");
     } else if (data === "03d" || data === "03n") {
-      setUrl("03d:n.webp");
+      setFilename("03d:n.webp");
     } else if (data === "03d" || data === "03n") {
-      setUrl("03d:n.webp");
+      setFilename("03d:n.webp");
     } else if (data === "04d" || data === "04n") {
-      setUrl("04d:n.jpg");
+      setFilename("04d:n.jpg");
     } else if (data === "09d" || data === "09n") {
-      setUrl("09d:n.jpg");
+      setFilename("09d:n.jpg");
     } else if (data === "10d") {
-      setUrl("10d.jpg");
+      setFilename("10d.jpg");
     } else if (data === "10n") {
-      setUrl("10n.jpg");
+      setFilename("10n.jpg");
     } else if (data === "11d" || data === "11n") {
-      setUrl("11d:n.jpg");
+      setFilename("11d:n.jpg");
     } else if (data === "13d" || data === "13n") {
-      setUrl("13d:n.jpeg");
+      setFilename("13d:n.jpeg");
     } else if (data === "50d" || data === "50n") {
-      setUrl("50d:n.jpg");
+      setFilename("50d:n.jpg");
     }
-    return url;
+    return filename;
   };
 
   useEffect(() => {
     backGround();
   }, []);
 
-  console.log(url);
+  console.log(filename);
   return (
-    <div id="poster" style={{ backgroundImage: `url(${url})` }}>
+    <div id="poster" style={{ backgroundImage: `url(${filename})` }}>
       <Nav />
       <Current image={backGround} />
     </div>

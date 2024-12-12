57f9d6c5c762d3149f9410247bd444a83af4dfa7
MI <minhazulislam@hotmail.com>
2024-12-12 21:17:19 +0000

    all images converted to jpg

--- src/App.jsx
+++ src/App.jsx
@@ -10,32 +10,34 @@ function App() {
   let [filename, setFilename] = useState("");
 
   const backGround = (data) => {
+    console.log(data)
+
     if (data === "01d") {
       setFilename("01d.jpg");
     } else if (data === "01n") {
       setFilename("01n.jpg");
     } else if (data === "02d") {
-      setFilename("02d.jpeg");
+      setFilename("02d.jpg");
     } else if (data === "02n") {
-      setFilename("02n.jpeg");
+      setFilename("02n.jpg");
     } else if (data === "03d" || data === "03n") {
-      setFilename("03d:n.webp");
+      setFilename("03d_n.jpg");
     } else if (data === "03d" || data === "03n") {
-      setFilename("03d:n.webp");
+      setFilename("03d_n.jpg");
     } else if (data === "04d" || data === "04n") {
-      setFilename("04d:n.jpg");
+      setFilename("04d_n.jpg");
     } else if (data === "09d" || data === "09n") {
-      setFilename("09d:n.jpg");
+      setFilename("09d_n.jpg");
     } else if (data === "10d") {
       setFilename("10d.jpg");
     } else if (data === "10n") {
       setFilename("10n.jpg");
     } else if (data === "11d" || data === "11n") {
-      setFilename("11d:n.jpg");
+      setFilename("11d_n.jpg");
     } else if (data === "13d" || data === "13n") {
-      setFilename("13d:n.jpeg");
+      setFilename("13d_n.jpg");
     } else if (data === "50d" || data === "50n") {
-      setFilename("50d:n.jpg");
+      setFilename("50d_n.jpg");
     }
      filename;
   };
